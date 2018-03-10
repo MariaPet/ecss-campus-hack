a16db570b84a3631ec41330e4393e08066b95616
panayiotis96 <panayiotisaristidou@hotmail.com>
2018-03-10 21:42:46 +0000

    weird commit

--- index.js
+++ index.js
@@ -41,13 +41,20 @@ app.post('/webhook', (req, res) => {
                     //sendTextMessage(sender, "Text received, echo: " + latitude + ","+longitude)
 
 
-                      sendTextMessage({
+
+                    quickReplies.push({
                         "content_type": "text",
-                        "title": "Next1",
+                        "title": "Next",
                         "payload": "Next"
                       });
 
-                    if( webhook_event.message.text === "Next1")
+                      sendTextMessage({
+                        "content_type": "text",
+                        "title": Next1,
+                        "payload": Next
+                      });
+
+                    if(webhook_event.message.quick_reply.payload === "Next")
                     {
                         var i = 0;
                         sendTextMessage("tEST");
@@ -82,17 +89,16 @@ app.post('/webhook', (req, res) => {
                             found = true;
                             results += JSON.stringify(body[i]);
                         }
-                        if (found === false){
-                            results = "Oh-oh I could't find any bus stops with this name ";
-                        }
-                        sendTextMessage(sender, "Text received, echo: " + results);
                         // else if (body[i].label.toLowerCase().indexOf(text.toLowerCase()) > 1) {
                         //     sendTextMessage(sender, "Great! I found several stops with that name, which one do you want?" + JSON.stringify(body[i]))
                         // }
-                    
-                    
                     }
-                });
+                    if (found === false){
+                        results = "Oh-oh I could't find any bus stops with this name ";
+                    }
+                    
+                    sendTextMessage(sender, "Text received, echo: " + results);
+                                    });
             }
             res.status(200).send('EVENT_RECEIVED');
         });
