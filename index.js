602fcc87bd3ea0cc26a893c50efe0dd4f169ba1e
mariapetriti <petriti.maria@gmail.com>
2018-03-10 20:07:40 +0200

    url for all bus stops

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.post('/webhook', (req, res) => {
         let sender = webhook_event.sender.id
         let text = webhook_event.message.text
 
-            request('https://transportapi.com/v3/uk/bus/stop/1980SN120405/live.json?app_id=552c4d0a&app_key=cf5a10e9aafbc058e660e49323985088&group=route&nextbuses=yes', function (error, response, body) {
+            request('http://data.southampton.ac.uk/dumps/bus-info/2018-03-04/stops.json', function (error, response, body) {
                 console.log('error:', error); // Print the error if one occurred
                 console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
                 
